3c16a030f2e4a7a04b8d357a3d93d875005996b5
Anil Unnikrishnan <unnikrishnan.anil@gmail.com>
2016-10-19 10:00:14 -0400

    Player class modification

--- 2D_Game_Engine/Game/player.cpp
+++ 2D_Game_Engine/Game/player.cpp
@@ -3,15 +3,15 @@
 Player::Player() : Entity("Character")
 {
     // Character
-    m_eCharacter = new Entity("Character");
-    m_eCharacter->transform->setPosition({100,100});
-    m_eCharacter->transform->setScale({1,1});
+    this->transform->setPosition({100,100});
+    this->transform->setScale({1,1});
 
     m_sSpriteComponent = new Sprite();
     m_sSpriteComponent->setName("Sprite_Component");
     m_sSpriteComponent->loadBMPFromString("/Users/anil/Downloads/Tutorials/30_scrolling/dot.bmp");
 
-    m_eCharacter->addComponent(m_sSpriteComponent);
+    this->addComponent(m_sSpriteComponent);
 
-    EntityManager::addEntity(m_eCharacter);
+    EntityManager::addEntity(this);
+
 }
